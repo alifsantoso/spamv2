81f1f9187a59e32dad17891228fbd16fcffada00
alifsantoso <86616463+alifsantoso@users.noreply.github.com>
2022-03-13 13:07:57 +0700

    Add files via upload

---
+++ spambrutal.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+
+#install project
+
+figlet loading | lolcat
+sleep 1
+
+git clone https://github.com/Inject-ID/spam-tsel
+
+figlet selesai | lolcat
+sleep 1
+
+cd spam-tsel
+
+
+#tampilan
+clear
+toilet -f big -F gay spam-brutal
+echo "=================================================" | lolcat
+echo " Author  : Alif Santoso"
+echo " Youtube : Panglima Selatan"
+echo " Github  : https://github.com/alifsantoso"
+echo "=================================================" | lolcat
+echo
+echo " Gunakan Dengan Bijak Ya"
+echo "=================================================" | lolcat
+
+php spam.php
